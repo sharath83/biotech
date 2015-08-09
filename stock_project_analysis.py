c1c36d53eca9968e83291c03ef9cc92fcf4b7855
Jason Lewris <datawrestler@gmail.com>
2015-08-08 22:42:26 -0400

    added percent change close capabilities

--- stock_project_analysis.py
+++ stock_project_analysis.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas.io.date_converters as conv
 import datetime
 from pandas.tseries.offsets import *
+import platform
 # the stocks in the csv file include AMGN, GILD, LLY, BIIB, PFE, GILD, BMS, and MRK
 
 # below are the dates of interest, the company, and the expected sign(postive or negative)
@@ -20,36 +21,68 @@ list = {'Event1': 'AMGN, 2013-03-19', 'Event2': 'GILD, 2013-03-05', 'Event3': 'L
 # first is to the retrieve the 10 days prior to the event and the 10 days after the event, including event date
 
 
-def stockhunter(event_date, symbol):
-    stocksdf = pd.read_csv('stocks_historical.csv', names = ['symbol', 'date', 'open', 'high', 'low', 'close', 'volume'],
+def stockhunter(eventnumber, event_date, symbol):
+    stocksdf = pd.read_csv('/Users/analyticsmachine/Desktop/MyLife/SchoolWork/UVA/CS_5010/project/stocks_historical.csv', names = ['symbol', 'date', 'open', 'high', 'low', 'close', 'volume'],
                        parse_dates = 'date',
                        infer_datetime_format = True,
                        keep_date_col = True,
                        index_col = 1)
+    # converting the string event_date into a datetime character
     a = pd.Series([pd.to_datetime(event_date)])
-    print(a[0])
+    # utilizing Pandas dateoffset which will look at the 10 days prior to the event_date
     startdate = a[0] + DateOffset(days=-10)
+    # utilizing Pandas dateoffset which will look at the 10 days following the event_date
     enddate = a[0] + DateOffset(days=10)
+    # converting back into a string so pandas locate feature can select between the range of dates
     startdate1 = str(startdate)
     enddate1 = str(enddate)
+    # removing the time section of the datetime
     startdate2 = startdate1[0:10]
+    # removing the time section of the datetime
     enddate2 = enddate1[0:10]
+    # searching between the range of dates
     keydate = stocksdf.loc[startdate2:enddate2]
     symbol_keydate = keydate[keydate['symbol'] == symbol]
+    # adding the event_number to the data frame symbol_keydate
+    symbol_keydate['Event_Number'] = eventnumber
+    # calculating the percent change from close to close
+    closedict = []
+    perchange = []
+    # calculating the percent change of close prices from one day to the next
+    for closeprice in symbol_keydate['close']:
+        if closedict == '':
+            closedict.append(closeprice)
+        else:
+            change = ((closedict / closeprice) - 1) * 100
+            perchange.append(change)
+            closedict = closeprice
+    symbol_keydate['percent_change_close'] = perchange
+    print(perchange)
     print(symbol_keydate)
-    try:
-        with open('C:\\Users\Jason\Desktop\MyLife\\UVA\CS_5010\Python\Python_Project\\biotech\output.csv', 'a') as f:
-            symbol_keydate.to_csv(f, header=False)
-    except:
-        symbol_keydate.to_csv('C:\\Users\Jason\Desktop\MyLife\\UVA\CS_5010\Python\Python_Project\\biotech\output.csv', header=True)
-stockhunter('2013-03-19', 'AMGN')
-stockhunter('2013-03-05', 'GILD')
+    # Adding this in so Jason L. can work from his laptop which is apple, push to github, pull on his desktop, and continue working in the windows environment
+    if platform.system() == 'Darwin':
+        try:
+            with open('/Users/analyticsmachine/Desktop/MyLife/SchoolWork/UVA/CS_5010/project/biotech/stockoutput.csv', 'a') as f:
+                symbol_keydate.to_csv(f, header=True)
+        except:
+            symbol_keydate.to_csv('/Users/analyticsmachine/Desktop/MyLife/SchoolWork/UVA/CS_5010/project/biotech/stockoutput.csv', header=True)
+    else:
+        try:
+            with open('C:\\Users\Jason\Desktop\MyLife\\UVA\CS_5010\Python\Python_Project\\biotech\output.csv', 'a') as f:
+                symbol_keydate.to_csv(f, header=True)
+        except:
+            symbol_keydate.to_csv('C:\\Users\Jason\Desktop\MyLife\\UVA\CS_5010\Python\Python_Project\\biotech\output.csv', header=True)
 
-for value in list.values():
-    test = value.split(',')
+
+for value in zip(list, list.values()):
+    test = value[1].split(',')
     symbol1 = test[0]
     event_date1 = test[1]
-    stockhunter(event_date1, symbol1)
+    event_number = value[0]
+    stockhunter(event_number, event_date1, symbol1)
+
+
+
 
 
 
